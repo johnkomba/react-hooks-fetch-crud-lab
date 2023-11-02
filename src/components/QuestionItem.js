7d8dc0ebdea52d147d10a72ec46951f289b4914d
John Komba <john.mwangi1@student.moringaschool.com>
2023-11-02 14:06:31 +0300

    done with assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, setQuestions }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,46 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setQuestions((prevQuestions) => prevQuestions.filter((q) => q.id !== id));
+      })
+      .catch((error) => console.error("Error deleting question:", error));
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select
+          defaultValue={correctIndex}
+          onChange={(e) => {
+            fetch(`http://localhost:4000/questions/${id}`, {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ correctIndex: e.target.value }),
+            })
+              .then(() => {
+                setQuestions((prevQuestions) =>
+                  prevQuestions.map((q) =>
+                    q.id === id ? { ...q, correctIndex: parseInt(e.target.value) } : q
+                  )
+                );
+              })
+              .catch((error) => console.error("Error updating question:", error));
+          }}
+        >
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
